aa713b2b7be2617c94717fc3151709fbc2500d64
PaoloTorregroza <paolodydtorregrosa@gmail.com>
2020-08-03 12:34:31 -0500

    Logs to files and helmet installed

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import "reflect-metadata";
-import {createConnection, Server} from "typeorm";
+import {createConnection} from "typeorm";
 import express from "express";
 import * as bodyParser from "body-parser";
 import routes from "./routes/routes";
 import cors from 'cors';
 import morgan from 'morgan';
+import fs from 'fs';
+import path from 'path';
+import helmet from 'helmet';
 
 const init = createConnection().then( async () => {
     try {
@@ -12,7 +15,14 @@ const init = createConnection().then( async () => {
         // create express app
         app.use(bodyParser.json());
         app.use(cors());
-        app.use(morgan("common"))
+        app.use(helmet());
+        
+        if (process.env.NODE_ENV == "production") {
+            const accessLogStream = fs.createWriteStream(path.join(__dirname, "logs/access.log"));
+            app.use(morgan("common", {stream: accessLogStream}))
+        } else {
+            app.use(morgan("common"));
+        }
 
         // register express routes from defined application routes
         app.use("/", routes);
